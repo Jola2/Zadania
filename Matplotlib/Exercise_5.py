958282b13c1605f2c1b843911b9eb7a24463a58a
Jola2 <jolazborek@gmail.com>
2022-05-18 22:11:10 +0200

    improving

--- Matplotlib/Exercise_5.py
+++ Matplotlib/Exercise_5.py
@@ -1,34 +1,25 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-grupa_1 = (21, 41, 22, 41, 42, 48, 20, 21, 20, 40, 40, 34, 21, 41, 47, 39, 43, 47, 28, 35, 49, 47, 22, 33, 30, 22, 29, 30, 41, 42)
-grupa_2 = (47, 49, 41, 31, 45, 24, 29, 22, 28, 24, 36, 28, 49, 40, 39, 39, 49, 26, 45, 26, 32, 30, 30, 41, 33)
-grupa_3 = (27, 44, 27, 40, 39, 42, 27, 28, 20, 30, 44, 38, 26, 37, 37, 27, 28, 30, 43, 37, 39, 26, 38, 47, 45, 40, 35)
+group_1= (21, 41, 22, 41, 42, 48, 20, 21, 20, 40, 40, 34, 21, 41, 47, 39, 43, 47, 28, 35, 49, 47, 22, 33, 30, 22, 29, 30, 41, 42)
+group_2= (47, 49, 41, 31, 45, 24, 29, 22, 28, 24, 36, 28, 49, 40, 39, 39, 49, 26, 45, 26, 32, 30, 30, 41, 33)
+group_3 = (27, 44, 27, 40, 39, 42, 27, 28, 20, 30, 44, 38, 26, 37, 37, 27, 28, 30, 43, 37, 39, 26, 38, 47, 45, 40, 35)
 
-srednia_1 = sum(grupa_1)/len(grupa_1)
-print('Średnia grupy_1:', srednia_1)
-print(min(grupa_1))
-print(max(grupa_1))
+avg_1 = sum(group_1)/len(group_1)
+avg_2 = sum(group_2)/len(group_2)
+avg_3 = sum(group_3)/len(group_3)
 
-srednia_2 = sum(grupa_2)/len(grupa_2)
-print('Średnia grupy_2:', srednia_2)
-print(min(grupa_2))
-print(max(grupa_2))
 
-srednia_3 = sum(grupa_3)/len(grupa_3)
-print('Średnia grupy_3:', srednia_3)
-print(min(grupa_3))
-print(max(grupa_3))
+avg_of_groups = (avg_1, avg_2, avg_3)
+min_groups = (min(group_1), min(group_2), min(group_3))
+max_groups = (max(group_1), max(group_2), max(group_3))
+range_of_plot = [(max(group_1)-min(group_1))/2, (max(group_2)-min(group_2))/2, (max(group_3)-min(group_3))/2]
 
-srednia_grup = (srednia_1, srednia_2, srednia_3)
-min_grup = (20, 22, 20)
-max_grup = (49, 49, 47)
 x = np.arange(3)
 plt.figure(figsize=(15,6))
-
-plt.bar(x, srednia_grup, width =0.3)
-plt.ylabel ('Punkty')
-plt.title ( 'Srednia liczba punktow w grupach')
-plt.xticks (x , ('Grupa 1', 'Grupa 2', 'Grupa 3') )
-plt.yticks ( np.arange (0 , 35 , 10))
+plt.bar(x, avg_of_groups, width =0.3, yerr=range_of_plot, capsize=6)
+plt.ylabel ('Points')
+plt.title ('The average number of points depending on groups')
+plt.xticks (x , ('Group 1', 'Group 2', 'Group 3') )
+plt.yticks ( np.arange (0 , 52, 3))
 plt.show()
