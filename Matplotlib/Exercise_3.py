1a522f9583532426c06449556d6a984175bbb4d0
Jola2 <100040541+Jola2@users.noreply.github.com>
2022-05-18 11:06:42 +0200

    Update Exercise_3.py

--- Matplotlib/Exercise_3.py
+++ Matplotlib/Exercise_3.py
@@ -3,10 +3,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 x = np.linspace(0, 1, 20)
-import matplotlib.pyplot as plt
-import numpy as np
-
-x = np.linspace(0, 1, 20)
 y1 = x
 y2 = x ** 2
 
