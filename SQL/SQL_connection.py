bf2208b57036e3ebcddd2f1bdfff46324113d08c
Jola2 <jolazborek@gmail.com>
2022-07-15 15:52:07 +0200

    Improving

---
+++ SQL/SQL_connection.py
@@ -0,0 +1,10 @@
+import mysql.connector
+from SQL.settings import user, password
+
+connection = mysql.connector.connect(host='localhost',
+                                     database='sales',
+                                     user=user,
+                                     password=password)
+
+print(connection.is_connected())
+
